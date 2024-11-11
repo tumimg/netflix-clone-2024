2b4962665f4475f6842e6c440d49024a5740beb8
tumimg <getachewtumim@gmail.com>
2024-11-10 21:38:10 -0800

    header and footer

--- netflix-clone-2024/src/App.js
+++ netflix-clone-2024/src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
 
 function App() {
   return (
     <div className="App">
-    <h1>hello</h1> 
+    <Header/> 
+    <Footer/> 
+
     </div>
   );
 }
---
+++ netflix-clone-2024/src/components/Header/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './header.css'; 
+import netflixlogo from '../../Assets/images/netflix logo.png'
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
+
+function Header() {
+  return (
+    <div className='header-outer-container'>
+      <div className='header-container'> 
+        <div className='header-left'>
+          <ul>
+          <li> img src={netflixlogo} alt="Netflixlogo" width="100</li>
+            <li>Netflix</li>
+            <li>Home</li>
+            <li>TV Shows</li> 
+            <li>Movies</li> 
+            <li>Latest</li>
+            <li>My List</li> 
+            <li>Browse by Languages</li> 
+          </ul>
+        </div>
+        <div className='header-right'> 
+          <ul>
+            <li><SearchIcon /></li>
+            <li><NotificationsNoneIcon /></li>
+            <li><AccountBoxIcon /></li>
+            <li><ArrowDropDownIcon /></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header; 
